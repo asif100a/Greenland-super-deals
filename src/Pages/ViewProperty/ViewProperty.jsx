cd6edbf926b7209c6768658d330abe7f3321b44f
N.M. Asif Sheikh <asifsheikhrrr@gmail.com>
2024-04-14 15:43:38 +0600

    Responsiveness for mobile device okey

--- src/Pages/ViewProperty/ViewProperty.jsx
+++ src/Pages/ViewProperty/ViewProperty.jsx
@@ -3,7 +3,6 @@ import { useLoaderData, useParams } from "react-router-dom";
 const ViewProperty = () => {
     const { id } = useParams();
     const data = useLoaderData();
-    console.log(data);
 
     const matched = data.find(current => current.id === parseInt(id));
     const {
@@ -21,10 +20,10 @@ const ViewProperty = () => {
     return (
         <div className="py-12 w-full">
             <div className="flex flex-col bg-[#f4ecf4a1] border border-gray-200 rounded-lg p-6 md:flex-row md:max-w-full">
-                <div className=" md:min-w-[40%]">
+                <div className="md:min-w-[40%]">
                     <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:rounded-none md:rounded-s-lg" src={image_url} alt="" />
                 </div>
-                <div className="flex flex-col justify-between ml-6 leading-normal">
+                <div className="flex flex-col justify-between md:ml-6 mt-3 md:mt-0 leading-normal">
                     <h1 className="mb-2 text-3xl font-bold tracking-tight text-gray-900">{estate_title}</h1>
                     <p className="mb-3 font-normal text-gray-700">{description}</p>
                     <div className="py-4 border-t-2 border-b-2 border-dashed mt-6 mb-6">
--- src/Shared/Navber/Navber.jsx
+++ src/Shared/Navber/Navber.jsx
@@ -15,21 +15,21 @@ const Navber = () => {
         <li><NavLink to={'/'}>Home</NavLink></li>
         <li><NavLink to={'/about_us'}>About us</NavLink></li>
         <li><NavLink to={'/contact_us'}>Contact us</NavLink></li>
-
     </>
 
     return (
-        <div className="navbar bg-base-100 shadow-md">
-            <div className="navbar-start">
+        <div className="navbar w-full bg-base-100 shadow-md">
+            <div className="w-full flex flex-row-reverse md:flex-row md:navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content right-0 mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                         {navLinks}
+                        <li>{ user ?<Link onClick={handleSingOut}>Log out</Link>  : <NavLink to={'/login'}>Login</NavLink>}</li>
                     </ul>
                 </div>
-                <Link to={'/'} className="text-2xl font-semibold hover:cursor-pointer flex justify-center items-center gap-1"><img src={logo} alt="" className="w-12 h-12 border border-green-600 bg-white" /><span>Greenland Super Deals</span></Link>
+                <Link to={'/'} className="text-2xl font-semibold hover:cursor-pointer flex justify-between md:justify-center items-center gap-1"><img src={logo} alt="" className="w-8 md:w-12 h-8 md:h-12 border border-green-600 bg-white" /><span className="">Greenland Super Deals</span></Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
@@ -38,11 +38,11 @@ const Navber = () => {
             </div>
 
 
-            <div className="navbar-end">
+            <div className="md:navbar-end">
                 {
                     user &&
                     <>
-                        <div className="profile mr-6">
+                        <div className="profile md:mr-6 hidden lg:flex">
                             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                                 <div className="w-10 rounded-full">
                                     <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
@@ -61,8 +61,8 @@ const Navber = () => {
                 }
 
                 {
-                    user ? <Link onClick={handleSingOut} className="btn">Log out</Link> :
-                        <Link to={'/login'} className="btn">Login</Link>
+                    user ? <Link onClick={handleSingOut} className="btn hidden lg:flex">Log out</Link> :
+                        <Link to={'/login'} className="btn hidden lg:flex">Login</Link>
                 }
             </div>
         </div>
